9acad8e9708ed69120d78c9b224edd37f8942cac
Dominik Schramm <d.schramm@dslan.at>
2020-03-28 23:31:44 +0100

    Frontend
     - Copy frontend files while building

---
+++ backend/src/main/java/com/medheroes/app/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.medheroes.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("nurse")
+                .password(encoder.encode("nurse"))
+                .roles("SERVICE")
+                .and()
+                .withUser("hospital")
+                .password(encoder.encode("hospital"))
+                .roles("CLIENT")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+    }
+}
